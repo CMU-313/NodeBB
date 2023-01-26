7c60bb88fc139a37fc6180d8ff234fded14664d0
Ryan Lin <ryanlin@ryans-mbp-5.wifi.local.cmu.edu>
2023-01-26 14:15:00 -0500

    Created promisify.ts file, created interfaces, and assigned types to main function

--- src/promisify.js
+++ src/promisify.js
@@ -1,8 +1,19 @@
-'use strict';
-
-const util = require('util');
-
-module.exports = function (theModule, ignoreKeys) {
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const util_1 = __importDefault(require("util"));
+function default_1(theModule, ignoreKeys) {
     ignoreKeys = ignoreKeys || [];
     function isCallbackedFunction(func) {
         if (typeof func !== 'function') {
@@ -11,51 +22,51 @@ module.exports = function (theModule, ignoreKeys) {
         const str = func.toString().split('\n')[0];
         return str.includes('callback)');
     }
-
     function isAsyncFunction(fn) {
         return fn && fn.constructor && fn.constructor.name === 'AsyncFunction';
     }
-
+    // Params are functions that have any possible args and outputs
+    function wrapCallback(origFn, callbackFn) {
+        return function wrapperCallback(...args) {
+            return __awaiter(this, void 0, void 0, function* () {
+                if (args.length && typeof args[args.length - 1] === 'function') {
+                    const cb = yield args.pop();
+                    args.push((err, res) => (res !== undefined ? cb(err, res) : cb(err)));
+                    return callbackFn(...args);
+                }
+                return origFn(...args);
+            });
+        };
+    }
+    // Params are functions that have any possible args and outputs
+    function wrapPromise(origFn, promiseFn) {
+        return function wrapperPromise(...args) {
+            if (args.length && typeof args[args.length - 1] === 'function') {
+                return origFn(...args);
+            }
+            return promiseFn(...args);
+        };
+    }
     function promisifyRecursive(module) {
         if (!module) {
             return;
         }
-
         const keys = Object.keys(module);
         keys.forEach((key) => {
             if (ignoreKeys.includes(key)) {
                 return;
             }
             if (isAsyncFunction(module[key])) {
-                module[key] = wrapCallback(module[key], util.callbackify(module[key]));
-            } else if (isCallbackedFunction(module[key])) {
-                module[key] = wrapPromise(module[key], util.promisify(module[key]));
-            } else if (typeof module[key] === 'object') {
+                module[key] = wrapCallback(module[key], util_1.default.callbackify(module[key]));
+            }
+            else if (isCallbackedFunction(module[key])) {
+                module[key] = wrapPromise(module[key], util_1.default.promisify(module[key]));
+            }
+            else if (typeof module[key] === 'object') {
                 promisifyRecursive(module[key]);
             }
         });
     }
-
-    function wrapCallback(origFn, callbackFn) {
-        return async function wrapperCallback(...args) {
-            if (args.length && typeof args[args.length - 1] === 'function') {
-                const cb = args.pop();
-                args.push((err, res) => (res !== undefined ? cb(err, res) : cb(err)));
-                return callbackFn(...args);
-            }
-            return origFn(...args);
-        };
-    }
-
-    function wrapPromise(origFn, promiseFn) {
-        return function wrapperPromise(...args) {
-            if (args.length && typeof args[args.length - 1] === 'function') {
-                return origFn(...args);
-            }
-
-            return promiseFn(...args);
-        };
-    }
-
     promisifyRecursive(theModule);
-};
+}
+exports.default = default_1;
