068ddec9f578aee2902152968e5b995fe1c23d58
ala <ala@aladembp.mynetworksettings.com>
2023-01-26 03:24:49 -0500

    converting list.js to list.ts

--- src/database/redis/list.js
+++ src/database/redis/list.js
@@ -1,57 +1,80 @@
-'use strict';
-
-module.exports = function (module) {
-    const helpers = require('./helpers');
-
-    module.listPrepend = async function (key, value) {
-        if (!key) {
-            return;
-        }
-        await module.client.lpush(key, value);
-    };
-
-    module.listAppend = async function (key, value) {
-        if (!key) {
-            return;
-        }
-        await module.client.rpush(key, value);
-    };
-
-    module.listRemoveLast = async function (key) {
-        if (!key) {
-            return;
-        }
-        return await module.client.rpop(key);
-    };
-
-    module.listRemoveAll = async function (key, value) {
-        if (!key) {
-            return;
-        }
-        if (Array.isArray(value)) {
-            const batch = module.client.batch();
-            value.forEach(value => batch.lrem(key, 0, value));
-            await helpers.execBatch(batch);
-        } else {
-            await module.client.lrem(key, 0, value);
-        }
-    };
-
-    module.listTrim = async function (key, start, stop) {
-        if (!key) {
-            return;
-        }
-        await module.client.ltrim(key, start, stop);
-    };
-
-    module.getListRange = async function (key, start, stop) {
-        if (!key) {
-            return;
-        }
-        return await module.client.lrange(key, start, stop);
-    };
-
-    module.listLength = async function (key) {
-        return await module.client.llen(key);
-    };
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const helpers_1 = __importDefault(require("./helpers"));
+function default_1(module) {
+    function listPrepend(key, value) {
+        return __awaiter(this, void 0, void 0, function* () {
+            if (!key) {
+                return;
+            }
+            yield module.client.lpush(key, value);
+        });
+    }
+    function listAppend(key, value) {
+        return __awaiter(this, void 0, void 0, function* () {
+            if (!key) {
+                return;
+            }
+            yield module.client.rpush(key, value);
+        });
+    }
+    function listRemoveLast(key) {
+        return __awaiter(this, void 0, void 0, function* () {
+            if (!key) {
+                return;
+            }
+            yield module.client.rpop(key);
+        });
+    }
+    function listRemoveAll(key, value) {
+        return __awaiter(this, void 0, void 0, function* () {
+            if (!key) {
+                return;
+            }
+            if (Array.isArray(value)) {
+                const batch = module.client.batch();
+                // The next line calls a function in a module that has not been updated to TS yet
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+                value.forEach(value => batch.lrem(key, 0, value));
+                yield helpers_1.default.execBatch(batch);
+            }
+            else {
+                yield module.client.lrem(key, 0, value);
+            }
+        });
+    }
+    function listTrim(key, start, stop) {
+        return __awaiter(this, void 0, void 0, function* () {
+            if (!key) {
+                return;
+            }
+            yield module.client.ltrim(key, start, stop);
+        });
+    }
+    function getListRange(key, start, stop) {
+        return __awaiter(this, void 0, void 0, function* () {
+            if (!key) {
+                return;
+            }
+            return yield module.client.lrange(key, start, stop);
+        });
+    }
+    function listLength(key) {
+        return __awaiter(this, void 0, void 0, function* () {
+            return yield module.client.llen(key);
+        });
+    }
+}
+exports.default = default_1;
---
+++ src/database/redis/list.ts
@@ -0,0 +1,61 @@
+import helpers from './helpers';
+
+export default function (module: { client: { lpush: (arg0: string, arg1:
+    number) => any; rpush: (arg0: string, arg1: number) => any; rpop:
+    (arg0: string)=> void | PromiseLike<void>; batch: () => any; lrem:
+    (arg0: string, arg1: number, arg2: number) => any; ltrim: (arg0: string,
+    arg1: number, arg2: number) => any; lrange: (arg0: string, arg1: number,
+    arg2: number) => any; llen: (arg0: string) => any; }; }) {
+    async function listPrepend(key:string, value:number):Promise<void> {
+        if (!key) {
+            return;
+        }
+        await module.client.lpush(key, value) as Promise<void>;
+    }
+
+    async function listAppend(key:string, value:number):Promise<void> {
+        if (!key) {
+            return;
+        }
+        await module.client.rpush(key, value)as Promise<void>;
+    }
+
+    async function listRemoveLast(key:string): Promise<void> {
+        if (!key) {
+            return;
+        }
+        await module.client.rpop(key) as void | Promise<void>;
+    }
+    async function listRemoveAll(key:string, value:number):Promise<void> {
+        if (!key) {
+            return;
+        }
+        if (Array.isArray(value)) {
+            const batch = module.client.batch();
+            // The next line calls a function in a module that has not been updated to TS yet
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+            value.forEach(value => batch.lrem(key, 0, value));
+            await helpers.execBatch(batch) as Promise<void>;
+        } else {
+            await module.client.lrem(key, 0, value) as Promise<void>;
+        }
+    }
+
+    async function listTrim(key:string, start:number, stop:number): Promise<void> {
+        if (!key) {
+            return;
+        }
+        await module.client.ltrim(key, start, stop)as Promise<number>;
+    }
+
+    async function getListRange(key:string, start:number, stop:number):Promise<number[]> {
+        if (!key) {
+            return;
+        }
+        return await module.client.lrange(key, start, stop)as Promise<number[]>;
+    }
+
+    async function listLength(key:string):Promise<number> {
+        return await module.client.llen(key)as Promise<number>;
+    }
+}
