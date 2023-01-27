e71d9cc01f7f8086a2283c8abd9a6ab29bf45397
zhihengy <zhihengy@andrew.cmu.edu>
2023-01-26 20:45:35 -0500

    lint ok

--- src/database/redis/connection.js
+++ src/database/redis/connection.js
@@ -1,53 +1,50 @@
-'use strict';
+"use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try {
-            step(generator.next(value));
-        }
-        catch (e) {
-            reject(e);
-        } }
-        function rejected(value) { try {
-            step(generator["throw"](value));
-        }
-        catch (e) {
-            reject(e);
-        } }
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
         function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-const nconf = require('nconf');
-const Redis = require('ioredis');
-const winston = require('winston');
-const connection = module.exports;
-connection.connect = function (options) {
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const nconf_1 = __importDefault(require("nconf"));
+const ioredis_1 = __importDefault(require("ioredis"));
+const winston_1 = __importDefault(require("winston"));
+function connect(options) {
     return __awaiter(this, void 0, void 0, function* () {
         return new Promise((resolve, reject) => {
-            options = options || nconf.get('redis');
+            // The next line calls a function in a module that has not been updated to TS yet
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+            options = options || nconf_1.default.get('redis');
             const redis_socket_or_host = options.host;
             let cxn;
             if (options.cluster) {
-                cxn = new Redis.Cluster(options.cluster, options.options);
+                cxn = new ioredis_1.default.Cluster(options.cluster, options.options);
             }
             else if (options.sentinels) {
-                cxn = new Redis(Object.assign({ sentinels: options.sentinels }, options.options));
+                cxn = new ioredis_1.default(Object.assign({ sentinels: options.sentinels }, options.options));
             }
             else if (redis_socket_or_host && String(redis_socket_or_host).indexOf('/') >= 0) {
                 // If redis.host contains a path name character, use the unix dom sock connection. ie, /tmp/redis.sock
-                cxn = new Redis(Object.assign(Object.assign({}, options.options), { path: redis_socket_or_host, password: options.password, db: options.database }));
+                cxn = new ioredis_1.default(Object.assign(Object.assign({}, options.options), { path: redis_socket_or_host, password: options.password, db: options.database }));
             }
             else {
                 // Else, connect over tcp/ip
-                cxn = new Redis(Object.assign(Object.assign({}, options.options), { host: redis_socket_or_host, port: options.port, password: options.password, db: options.database }));
+                cxn = new ioredis_1.default(Object.assign(Object.assign({}, options.options), { host: redis_socket_or_host, port: options.port, password: options.password, db: options.database }));
             }
             const dbIdx = parseInt(options.database, 10);
             if (!(dbIdx >= 0)) {
                 throw new Error('[[error:no-database-selected]]');
             }
             cxn.on('error', (err) => {
-                winston.error(err.stack);
+                // The next line calls a function in a module that has not been updated to TS yet
+                // eslint-disable-next-line  @typescript-eslint/no-unsafe-member-access
+                winston_1.default.error(err.message);
                 reject(err);
             });
             cxn.on('ready', () => {
@@ -56,9 +53,9 @@ connection.connect = function (options) {
                 resolve(cxn);
             });
             if (options.password) {
-                cxn.auth(options.password);
+                cxn.auth(options.password).catch(() => 'obligatory catch');
             }
         });
     });
-};
-require('../../promisify')(connection);
+}
+exports.default = connect;
--- src/database/redis/connection.ts
+++ src/database/redis/connection.ts
@@ -1,54 +1,79 @@
-'use strict';
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-const nconf = require('nconf');
-const Redis = require('ioredis');
-const winston = require('winston');
-const connection = module.exports;
-connection.connect = function (options) {
-    return __awaiter(this, void 0, void 0, function* () {
-        return new Promise((resolve, reject) => {
-            options = options || nconf.get('redis');
-            const redis_socket_or_host = options.host;
-            let cxn;
-            if (options.cluster) {
-                cxn = new Redis.Cluster(options.cluster, options.options);
-            }
-            else if (options.sentinels) {
-                cxn = new Redis(Object.assign({ sentinels: options.sentinels }, options.options));
-            }
-            else if (redis_socket_or_host && String(redis_socket_or_host).indexOf('/') >= 0) {
-                // If redis.host contains a path name character, use the unix dom sock connection. ie, /tmp/redis.sock
-                cxn = new Redis(Object.assign(Object.assign({}, options.options), { path: redis_socket_or_host, password: options.password, db: options.database }));
-            }
-            else {
-                // Else, connect over tcp/ip
-                cxn = new Redis(Object.assign(Object.assign({}, options.options), { host: redis_socket_or_host, port: options.port, password: options.password, db: options.database }));
-            }
-            const dbIdx = parseInt(options.database, 10);
-            if (!(dbIdx >= 0)) {
-                throw new Error('[[error:no-database-selected]]');
-            }
-            cxn.on('error', (err) => {
-                winston.error(err.stack);
-                reject(err);
+import nconf from 'nconf';
+import Redis, { ChainableCommander, Cluster, ClusterNode, ClusterOptions, SentinelAddress } from 'ioredis';
+import winston from 'winston';
+import { Transaction } from 'ioredis/built/transaction';
+
+
+type Options = {
+    sentinels:Partial<SentinelAddress>[];
+    cluster:ClusterNode[];
+    host: string;
+    options: ClusterOptions;
+    password: string;
+    database: number | string;
+    port: number;
+}
+
+type Cxn = {
+    batch : ChainableCommander;
+    pipeline : ChainableCommander;
+}
+
+export default async function connect(options: Options):Promise<void> {
+    return new Promise((resolve, reject) => {
+        // The next line calls a function in a module that has not been updated to TS yet
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        options = options || nconf.get('redis');
+        const redis_socket_or_host = options.host;
+
+        let cxn: Cluster | Redis |Transaction|Cxn |void;
+        if (options.cluster) {
+            cxn = new Redis.Cluster(options.cluster, options.options);
+        } else if (options.sentinels) {
+            cxn = new Redis({
+                sentinels: options.sentinels,
+                ...options.options,
             });
-            cxn.on('ready', () => {
-                // back-compat with node_redis
-                cxn.batch = cxn.pipeline;
-                resolve(cxn);
+        } else if (redis_socket_or_host && String(redis_socket_or_host).indexOf('/') >= 0) {
+            // If redis.host contains a path name character, use the unix dom sock connection. ie, /tmp/redis.sock
+            cxn = new Redis({
+                ...options.options,
+                path: redis_socket_or_host,
+                password: options.password,
+                db: options.database as number,
             });
-            if (options.password) {
-                cxn.auth(options.password);
-            }
+        } else {
+            // Else, connect over tcp/ip
+            cxn = new Redis({
+                ...options.options,
+                host: redis_socket_or_host,
+                port: options.port,
+                password: options.password,
+                db: options.database as number,
+            });
+        }
+
+        const dbIdx:number = parseInt(options.database as string, 10);
+        if (!(dbIdx >= 0)) {
+            throw new Error('[[error:no-database-selected]]');
+        }
+
+        (cxn as Cluster).on('error', (err) => {
+            // The next line calls a function in a module that has not been updated to TS yet
+            // eslint-disable-next-line  @typescript-eslint/no-unsafe-member-access
+            winston.error(err.message);
+            reject(err);
         });
+        (cxn as Cluster).on('ready', () => {
+            // back-compat with node_redis
+            (cxn as Cxn).batch = (cxn as Cxn).pipeline;
+            resolve(cxn as void);
+        });
+
+        if (options.password) {
+            (cxn as Cluster).auth(options.password).catch(() => 'obligatory catch');
+        }
     });
-};
-require('../../promisify')(connection);
+}
+
+
