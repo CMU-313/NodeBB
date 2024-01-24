f348beb29afc748866e18557c19b83f964f45fc9
Chaeil Yun <chaeily@andrew.cmu.edu>
2024-01-24 12:28:48 -0500

    more error fixing, this time with export at the end...

--- src/upgrades/1.0.0/chat_upgrade.ts
+++ src/upgrades/1.0.0/chat_upgrade.ts
@@ -1,85 +1,101 @@
-'use strict';
-
-import * as async from 'async';
-import * as winston from 'winston';
-import * as db from '../../database';
-
-interface Message {
-    fromuid: string;
-    touid: string;
-    timestamp: string;
-}
+import async from 'async';
+import winston from 'winston';
+import db from '../../database';
 
 interface GlobalData {
-    nextMid: number;
-    nextChatRoomId: number;
+  nextMid: number;
+  nextChatRoomId: number;
+}
+
+interface Message {
+  fromuid: string;
+  touid: string;
+  timestamp: string;
 }
 
 interface Rooms {
-    [pairID: string]: number;
+  [key: string]: number;
 }
 
-export = {
-    name: 'Upgrading chats',
-    timestamp: Date.UTC(2015, 11, 15),
-    method: function (callback: (err: Error | null) => void) {
-        db.getObjectFields('global', ['nextMid', 'nextChatRoomId'], (err: Error | null, globalData?: GlobalData) => {
-            if (err) {
-                return callback(err);
+const upgradeChats = {
+  name: 'Upgrading chats',
+  timestamp: Date.UTC(2015, 11, 15),
+  method: function (callback: (err?: Error) => void) {
+    db.getObjectFields('global', ['nextMid', 'nextChatRoomId'], (err: Error, globalData: GlobalData) => {
+      if (err) {
+        return callback(err);
+      }
+
+      const rooms: Rooms = {};
+      let roomId: number = globalData.nextChatRoomId || 1;
+      let currentMid: number = 1;
+
+      async.whilst(
+        (next: (err: Error | null, result: boolean) => void) => {
+          next(null, currentMid <= globalData.nextMid);
+        },
+        (next: (err?: Error) => void) => {
+          db.getObject(`message:${currentMid}`, (err: Error, message: Message) => {
+            if (err || !message) {
+              winston.verbose('skipping chat message ', currentMid);
+              currentMid += 1;
+              return next(err);
             }
 
-            const rooms: Rooms = {};
-            let roomId = globalData?.nextChatRoomId || 1;
-            let currentMid = 1;
+            const pairID: string = [parseInt(message.fromuid, 10), parseInt(message.touid, 10)].sort().join(':');
+            const msgTime: number = parseInt(message.timestamp, 10);
 
-            async.whilst((next: (err: Error | null, result: boolean) => void) => {
-                next(null, currentMid <= (globalData?.nextMid || 0));
-            }, (next: (err: Error | null) => void) => {
-                db.getObject(`message:${currentMid}`, (err: Error | null, message?: Message) => {
-                    if (err || !message) {
-                        winston.verbose('skipping chat message ', currentMid);
-                        currentMid += 1;
-                        return next(err);
-                    }
+            function addMessageToUids(roomId: number, callback: (err?: Error) => void) {
+              async.parallel([
+                function (next) {
+                  db.sortedSetAdd(`uid:${message.fromuid}:chat:room:${roomId}:mids`, msgTime, currentMid, next);
+                },
+                function (next) {
+                  db.sortedSetAdd(`uid:${message.touid}:chat:room:${roomId}:mids`, msgTime, currentMid, next);
+                },
+              ], callback);
+            }
 
-                    const pairID = [parseInt(message.fromuid, 10), parseInt(message.touid, 10)].sort().join(':');
-                    const msgTime = parseInt(message.timestamp, 10);
-
-                    function addMessageToUids(roomId: number, callback: (err: Error | null) => void) {
-                        async.parallel([
-                            (next) => db.sortedSetAdd(`uid:${message.fromuid}:chat:room:${roomId}:mids`, msgTime, currentMid, next),
-                            (next) => db.sortedSetAdd(`uid:${message.touid}:chat:room:${roomId}:mids`, msgTime, currentMid, next),
-                        ], callback);
-                    }
-
-                    if (rooms[pairID]) {
-                        winston.verbose(`adding message ${currentMid} to existing roomID ${roomId}`);
-                        addMessageToUids(rooms[pairID], (err) => {
-                            if (err) {
-                                return next(err);
-                            }
-                            currentMid += 1;
-                            next(err);
-                        });
-                    } else {
-                        winston.verbose(`adding message ${currentMid} to new roomID ${roomId}`);
-                        async.parallel([
-                            (next) => db.sortedSetAdd(`uid:${message.fromuid}:chat:rooms`, msgTime, roomId, next),
-                            (next) => db.sortedSetAdd(`uid:${message.touid}:chat:rooms`, msgTime, roomId, next),
-                            (next) => db.sortedSetAdd(`chat:room:${roomId}:uids`, [msgTime, msgTime + 1], [message.fromuid, message.touid], next),
-                            (next) => addMessageToUids(roomId, next),
-                        ], (err) => {
-                            if (err) {
-                                return next(err);
-                            }
-                            rooms[pairID] = roomId;
-                            roomId += 1;
-                            currentMid += 1;
-                            db.setObjectField('global', 'nextChatRoomId', roomId, next);
-                        });
-                    }
-                });
-            }, callback);
-        });
-    },
+            if (rooms[pairID]) {
+              winston.verbose(`adding message ${currentMid} to existing roomID ${roomId}`);
+              addMessageToUids(rooms[pairID], (err) => {
+                if (err) {
+                  return next(err);
+                }
+                currentMid += 1;
+                next();
+              });
+            } else {
+              winston.verbose(`adding message ${currentMid} to new roomID ${roomId}`);
+              async.parallel([
+                function (next) {
+                  db.sortedSetAdd(`uid:${message.fromuid}:chat:rooms`, msgTime, roomId, next);
+                },
+                function (next) {
+                  db.sortedSetAdd(`uid:${message.touid}:chat:rooms`, msgTime, roomId, next);
+                },
+                function (next) {
+                  db.sortedSetAdd(`chat:room:${roomId}:uids`, [msgTime, msgTime + 1], [message.fromuid, message.touid], next);
+                },
+                function (next) {
+                  addMessageToUids(roomId, next);
+                },
+              ], (err) => {
+                if (err) {
+                  return next(err);
+                }
+                rooms[pairID] = roomId;
+                roomId += 1;
+                currentMid += 1;
+                db.setObjectField('global', 'nextChatRoomId', roomId, next);
+              });
+            }
+          });
+        },
+        callback
+      );
+    });
+  },
 };
+
+export default upgradeChats;
