f1625072ab1bb3fc8d7d91ae22eae3adc64a2e4c
Minh <minhdo@cmu.edu>
2026-01-24 04:35:28 +0000

    removed temporary logs

--- src/posts/votes.js
+++ src/posts/votes.js
@@ -122,7 +122,6 @@ module.exports = function (Posts) {
 	}
 
 	async function unvote(pid, uid, type) {
-		console.log('Minh');
 		const voteStatus = await Posts.hasVoted(pid, uid);
 		const owner = await Posts.getPostField(pid, 'uid');
 		if (parseInt(uid, 10) === parseInt(owner, 10)) {
